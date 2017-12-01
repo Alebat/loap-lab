36f48b116bb00543ba89bcb9d6a015ec654b9043
dpaissan <dpaissan@fbk.eu>
2017-12-01 12:06:18 +0100

    hehehe real data

---
+++ end_data/splitter.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def oo(fin, fout, num, den):
+    with open(fin, "r") as f:
+        with open(fout, "w") as fw:
+            for index, line in enumerate(f):
+                if index % den == num:
+                    fw.write(line)
+
+
+oo(sys.argv[1], sys.argv[1]+'0', 0, 3)
+oo(sys.argv[1], sys.argv[1]+'1', 1, 3)
+oo(sys.argv[1], sys.argv[1]+'2', 2, 3)
+
+
+first_val = None
+with open(sys.argv[1]+'2', "r") as f:
+    with open(sys.argv[1]+'2f', "w") as fw:
+        for index, line in enumerate(f):
+            time = int(line.split(" ")[0])
+            val = int(line.split(" ")[1].rstrip())
+            if val > 0 and time > 400:
+                if first_val is None:
+                    first_val = val
+                fw.write(line.split(" ")[0] + " " + str(val - first_val) + "\n")
--- poli.m
+++ poli.m
@@ -1,31 +1,31 @@
-function [P1,P2,k] = poli(data,dt)
+function [P1,P2,k] = poli(data,dt, t, bplot)
 
 time = data(1:length(data)-1, 1) / 1000;
 angle = data(:, 2) / 180 * 3.141592653595 / 65.5;
 speed = diff(angle) / dt;
 sspeed = speed;
 
-% windowSize = 8;
-% sspeed = filter((1/windowSize)*ones(1,windowSize), 1, sspeed);
-sspeed = expmean(sspeed, .64);
+windowSize = 8;
+sspeed = filter((1/windowSize)*ones(1,windowSize), 1, sspeed);
+% sspeed = expmean(sspeed, .81);%.64);
 
-hold on;
-plot(time, speed);
-plot(time, sspeed);
+if bplot == 1
+    hold on;
+    plot(time, speed);
+    plot(time, sspeed);
+end
 
 time_stable = .5;
 time_stable_filtered = time_stable;
 y_inf = mean(sspeed(time_stable_filtered/dt));
 
-t = 0.05;
+t1 = floor(t/dt);
+t2 = floor(2*t/dt);
+t3 = floor(3*t/dt);
 
-t1 = floor(t/dt)
-t2 = floor(2*t/dt)
-t3 = floor(3*t/dt)
-
-y_t1 = sspeed(t1)
-y_t2 = sspeed(t2)
-y_t3 = sspeed(t3)
+y_t1 = sspeed(t1);
+y_t2 = sspeed(t2);
+y_t3 = sspeed(t3);
 
 line([t t], [0 .2]);
 line(2*[t t], [0 .2]);
@@ -41,8 +41,8 @@ b = 4*k1^3*k3 - 3*k1^2*k2^2 - 4*k2^3 + k3^2 + 6*k1*k2*k3;
 a1 = (k1*k2 + k3 - sqrt(b)) / (2*(k1^2 + k2));
 a2 = (k1*k2 + k3 + sqrt(b)) / (2*(k1^2 + k2));
 
-P1 = log(a1)/t
-P2 = log(a2)/t
+P1 = log(a1)/t;
+P2 = log(a2)/t;
 
 end
 
